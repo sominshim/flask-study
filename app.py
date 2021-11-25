887f8541870e6a80e826ad4f3eda95b89f096e29
somin <shimsomin20@gmail.com>
2021-11-25 23:47:09 +0900

    Learn Form with WTF

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField # 텍스트 입력.. 제출 등에 필요함
+from wtforms.validators import DataRequired # 유효성 검사
 
+# Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
+
+# Create a From Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit =SubmitField("Submit")
+# learn more
+# https://flask-wtf.readthedocs.io/en/1.0.x/
 
 @app.route('/')
 def index():
@@ -22,3 +34,16 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validation Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template("name.html",
+        name = name,
+        form = form)
