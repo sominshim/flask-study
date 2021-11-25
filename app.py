b9d9309f07c008182bd7abb3794751bd55590ba9
somin <shimsomin20@gmail.com>
2021-11-25 22:32:37 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    first_name = "John"
+    return render_template("index.html", first_name = first_name)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", user_name = name)
+
+# Create Custom Error Pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
